d69c54f3f9aab8291bb357837b0c857d84160d78
Thophylarh <iwualathophylarh@gmail.com>
2024-06-28 17:05:16 +0100

    my approach section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+
+
+import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
@@ -12,14 +15,13 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hiden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={navItems}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
-        <Clients/>
-        <Experience/>
+        <Clients />
+        <Experience />
+        <Approach />
       </div>
     </main>
   );
